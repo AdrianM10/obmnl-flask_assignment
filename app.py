62663652a7ba65800c92b3238f650a1747f53670
Adrian Mudzwiti <54492734+AdrianM10@users.noreply.github.com>
2025-06-11 21:35:06 +0200

    Create new transaction

--- app.py
+++ app.py
@@ -10,12 +10,31 @@ transactions = [
     {"id": 3, "date": "2023-06-03", "amount": 300},
 ]
 
-# Read operation
 @app.route("/")
 def get_transactions():
+    """Retrieve all transactions"""
     return render_template("transactions.html", transactions=transactions)
 
-# Create operation
+@app.route("/add", methods=["GET", "POST"])
+def add_transaction():
+    """Add new transaction(s)"""
+
+    # Check if the request method is POST (form submission)
+    if request.method == "POST":
+        transaction = {
+            "id": len(transactions) + 1,
+            "date": request.form["date"],
+            "amount": float(request.form["amount"])
+        }
+
+        # Append new transaction to the transaction list
+        transactions.append(transaction)
+
+        # Redirect to the transactions list page after adding the new transaction
+        return redirect(url_for("get_transactions"))
+    
+    # If the request method is GET, render the form template to display the add transaction form
+    return render_template("form.html")
 
 # Update operation
 
